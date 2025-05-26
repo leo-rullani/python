e560dc7958838243ad3a5d33f1a44b0aa4902e71
Leugzim Rullani <leugzimrullani@outlook.com>
2025-05-26 23:29:28 +0200

    inkrement und dekrement

---
+++ theorie/inkrement_dekrement.py
@@ -0,0 +1,23 @@
+# i        = 0
+# i++      <- Wert 1
+# i+ +     <- Wert 1
+# i++      <- Wert 1
+
+# Wie realisieren wir dieses Verhalten in Python? 
+# i  = 0 
+# i += 1
+# i -= 1
+
+# ODER 
+i  = 6
+i /= 2
+
+# ODER 
+a = 5 
+a **= 2
+print(a) # 25
+
+# ODER
+b = 10 
+b %= 3
+print (b) # 1
