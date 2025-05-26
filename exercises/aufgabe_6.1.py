3e769a989e9ce38c5237e95b5cb39849e6620a44
Leugzim Rullani <leugzimrullani@outlook.com>
2025-05-26 23:17:33 +0200

    arithmetische operatoren

---
+++ exercises/aufgabe_6.1.py
@@ -0,0 +1,25 @@
+# Was kommt bei den folgenden Rechnungen als Ergebnis heraus?
+12 * 12                                                     # 144
+2.5 + 3.5                                                   # 6.0 (Addition von Floats = Float)
+20 / 5                                                      # 4.0
+15 – 3 * 5                                                  # 0
+8 + 10//4                                                   # 10 (Integer-Divison, nur Vor-Komma-Teil wird berücksichtigt)
+8 + 10/4                                                    # 10.5
+20 / (5 – 4)                                                # 20.0
+4 * 2.5 / 4                                                 # 2.5
+1//2 - 3//4                                                 # 0
+(12 % 5) * (5 % 12)                                         # 10 (5/12 = 0, Rest 5)
+2**3                                                        # 8
+4**(8 - 5)                                                  # 64
+2**(8 // 2)                                                 # 16
+5**2 * 3                                                    # 75
+(8 – 5)**(27 // 9)                                          # 27
+"apfel" + "baum"                                            # apfelbaum
+5 * ("a" + "b")                                             # ababababab
+"x" * 2**3                                                  # xxxxxxxx
+12 % 5                                                      # 2
+42 % 43                                                     # 42
+2**10 % 2                                                   # 0 (1024/2)
+5 * 4 * 3 * 2 * 1.0                                         # 120.0
+0 * "Florian"                                               # ""
+"xyz" * (12 % 5)                                            # xyzxyz
---
+++ theorie/arithmetische_operatoren.py
@@ -0,0 +1,30 @@
+#DIVISON
+print(30/15)        # 2.0
+print(30//15)       # 2 
+
+print(3/10)         # 0.3
+print(3//10)        # 0
+
+#MODULU-OPERATOR
+print(42 % 12)      # 6 - 36:12 = 3, 42 - 36 = 6
+print(10 % 2)       # 0 - 10:2 = 5
+
+#POTENZIEREN
+print(2**5)         # 32
+
+#MATHEMATIK-GRUNDREGEL (PUNKT VOR STRICH)
+print(2 * 5 + 3)    # 13
+print(2 + 5 * 3)    # 17
+
+print(10 / 2 + 5)   # 10.0
+print(5 + 10 / 2)   # 10.0
+
+#GEMISCHTE ÜBUNGEN
+5**3 + 2            # 127
+4 + 3 - 5           # 2
+6 + 8 / 2           # 10.0
+10 * 2**3           # 80
+5 * 9 * 2 / 10      # 9.0
+5 % 3 * 7           # 14
+6 % 5 + 3           # 4
+10 % 2**2           # 2
