aac3e0f186a0cd3ed9af31a8ff821055f09ba83f
Leugzim Rullani <leugzimrullani@outlook.com>
2025-05-27 07:13:07 +0200

    logische operatoren exercises

--- theorie/logische_operatoren.py
+++ theorie/logische_operatoren.py
@@ -1,9 +1,19 @@
-# x or - Verknüpfung (^)
+# XOR - Verknüpfung (^)
 
 # 10101 # <- Klartext
 # 11011 # <- Schlüssel ^
-#  -----
+# -----
 # 01110 # <- Verschlüsselte Klartext
 # 11011 # <- Schlüssel ^
 # -----
 # 10101 # <- Verschlüsselte Klartext
+
+print(2 < 3 and 3 < 5)              # True
+print(2 < 3 and 3 == 5)             # False
+print(2 < 3 or 3 == 5)              # True
+print(2 < 3 and 3 != 5)             # True
+print((2 < 3) ^ (3 != 5))           # False
+print((2 < 3) ^ (3 == 5))           # True
+print (True or 2 < 3 and False)     # True
+print (True and 2 < 3 and False)    # False
+print ((True or 2 < 3) and False)   # False
