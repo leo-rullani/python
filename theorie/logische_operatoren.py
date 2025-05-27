7a37d0b61a9108ab6ec42056e1cfe742443bf37c
Leugzim Rullani <leugzimrullani@outlook.com>
2025-05-27 06:50:50 +0200

    logische operatoren

---
+++ theorie/logische_operatoren.py
@@ -0,0 +1,9 @@
+# x or - Verknüpfung (^)
+
+# 10101 # <- Klartext
+# 11011 # <- Schlüssel ^
+#  -----
+# 01110 # <- Verschlüsselte Klartext
+# 11011 # <- Schlüssel ^
+# -----
+# 10101 # <- Verschlüsselte Klartext
