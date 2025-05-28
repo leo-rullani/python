fb1925b45d9afc9eeba1e587566637a14309e9d6
Leugzim Rullani <leugzimrullani@outlook.com>
2025-05-28 12:15:25 +0200

    listen

---
+++ theorie/listen.py
@@ -0,0 +1,8 @@
+liste = [29, "Leugzim", True]
+print(len(liste))                   # Ausgabe: 3 (Elemente)
+
+gerade = [0, 2, 4, 6, 8, 10]
+print(len(gerade))                   # Ausgabe: 6 (Elemente)
+
+print(gerade[2])                     # Ausgabe: 4 (Index) auch mit [-4] möglich.
+print(gerade[2:5])                   # Ausgabe: [4, 6, 8]
