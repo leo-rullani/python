317189c686b4e4ded77011faad28594603bf56e7
Leugzim Rullani <leugzimrullani@outlook.com>
2025-05-29 13:47:38 +0200

    tryandexcept

---
+++ theorie/fehlerbehandlung/try&except.py
@@ -0,0 +1,7 @@
+liste = [1, 2, 3, "Leo", "Sabit", "Fatmire"]
+
+try:
+    index = int(input("Bitte gib einen Index ein: "))
+    print(liste[index])
+except Exception as ex: 
+    print(f"Es ist ein Fehler aufgetreten.\n{ex}")
